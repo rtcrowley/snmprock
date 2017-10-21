e49476052fab6bdfd0c24e124fa8f1a5a8f89bca
rtcrowley <crowley.rtc@gmail.com>
2017-10-20 21:52:12 -0700

    Update snmprock.sh

--- snmprock.sh
+++ snmprock.sh
@@ -19,14 +19,14 @@ usage=$(
     echo "-----------------------------------------------------------------------")
 
 if [ -z "$1" ] || [[ "$1" != "-"* ]]; then
-        echo "${usage}"
-        exit 1
+	echo "${usage}"
+	exit 1
 fi
 
 c='public'
 
 echo "-----------------------------------------------------------------------"
-echo "-------------------------------snmprock--------------------------------"
+echo "------------------------------snmprock-------------------------------"
 
 
 
@@ -34,179 +34,161 @@ while getopts ':c:t:akonhb:' flag; do
   case "${flag}" in
 
     c) c="${OPTARG}"
-        ;;
+	;;
     t) t="${OPTARG}"
-        echo "-----------------------------------------------------------------------"
-        echo "Community String set to: ${c}"
-        echo "-----------------------------------------------------------------------"
-        echo "Acquiring Target ${t}...."
-        nmap=$(nmap -sU -p161 $t)
-         case "$nmap" in
-          *open*)
-             echo "Target Acquired." 
-             echo "Nmap shows SNMP OPEN for business"
-             check=$(snmpwalk -v1 -c $c $t .1.3.6.1.2.1.1.1.0)
+	echo "-----------------------------------------------------------------------"
+	echo "Community String set to: ${c}"
+	echo "-----------------------------------------------------------------------"
+	echo "Acquiring Target ${t}...."
+	nmap=$(nmap -sU -p161 $t)
+	 case "$nmap" in
+	  *open*)
+	     echo "Target Acquired." 
+	     echo "Nmap shows SNMP OPEN for business"
+	     check=$(snmpwalk -v1 -c $c $t .1.3.6.1.2.1.1.1.0)
 
-             if [ -z "$check" ]
-             then
-                echo "-----------------------------------------------------------------------"
-                echo "---------------------snmpwalk connection FAILED------------------------"
-                echo "-------------------Double check Community String-----------------------"
-                exit 1
-             else
-                echo "snmpwalk connection SUCCESSFUL. Enuming now..."
-             fi
-             
-             #Storing target host resources in tmp txt file.
-             snmpwalk -v1 -c $c $t .1.3.6.1.2.1.25.6.3.1.2 |cut -d ':' -f2 |cut  -d '"' -f 2 >> snmp_tmp.txt
-             
-             if [ -s snmp_tmp.txt ]
-             then
-                echo "Host resources captured...."
-             else
-                echo "--No host resources captured. SNMP on target may be limited. Exiting.--"
-                rm snmp_tmp.txt
-                echo "-----------------------------------------------------------------------"
-                exit 1
-             fi
-             echo "-----------------------------------------------------------------------"
-             ;;
-           *)
-             echo "------------------SNMP seems to be closed or filtered------------------"
-             exit 1
-             ;;
-         esac
+	     if [ -z "$check" ]
+	     then
+		echo "-----------------------------------------------------------------------"
+		echo "---------------------snmpwalk connection FAILED------------------------"
+		echo "-------------------Double check Community String-----------------------"
+		echo "-----------------------------------------------------------------------"
+		exit 1
+	     else
+		echo "snmpwalk connection SUCCESSFUL. Enuming now..."
+	     fi
+	     #Getting system resources. Saving to temp file.
+	     snmpwalk -v1 -c $c $t .1.3.6.1.2.1.25.6.3.1.2 |cut -d ':' -f2 |cut  -d '"' -f 2 >> snmp_tmp.txt
+	     if [ -s snmp_tmp.txt ]
+	     then
+		echo "Host resources captured...."
+	     else
+		echo "--No host resources captured. SNMP on target may be limited. Exiting.--"
+		rm snmp_tmp.txt
+		echo "-----------------------------------------------------------------------"
+		exit 1
+	     fi
+	     echo "-----------------------------------------------------------------------"
+	     ;;
+	   *)
+	     echo "------------------SNMP seems to be closed or filtered------------------"
+	     exit 1
+	     ;;
+	 esac
        ;;
     a) proc=$(snmpwalk -v1 -c public $t .1.3.6.1.2.1.25.4.2.1.2 |cut -d ':' -f2 |cut  -d '"' -f2)
-        echo "-----------------------------------------------------------------------"
-        echo "------------------------Do your due diligence--------------------------"
-        echo "---------------------------ACTIVE processes----------------------------"
-        echo "-----------------Melding out host resource version intel---------------"
-        echo "-----------------------------------------------------------------------"
-        
-        #Matches running process names to stored process verisons in snmp_tmp.txt
-        grep -r "${proc}" "snmp_tmp.txt" |sort -u
-        osinfo=$(snmpwalk -v1 -On $t -c $c .1.3.6.1.2.1.1.1.0 |cut -d ':' -f2 |cut -d '#' -f1 |cut -d '"' -$
-        echo "-----------------------------------------------------------------------"
-        echo "----------------Try manual searchsploit queries using------------------"
-        echo "------------------ ACTIVE process list from above----------------------"
-        echo "-----------------------------------------------------------------------"
+	echo "-----------------------------------------------------------------------"
+	echo "------------------------Do your due diligence--------------------------"
+	echo "---------------------------ACTIVE processes----------------------------"
+	echo "-----------------Melding out host resource version intel---------------"
+	echo "-----------------------------------------------------------------------"
+	grep -r "${proc}" "snmp_tmp.txt" |sort -u
+	osinfo=$(snmpwalk -v1 -On $t -c $c .1.3.6.1.2.1.1.1.0 |cut -d ':' -f2 |cut -d '#' -f1 |cut -d '"' -f2)
+	echo "-----------------------------------------------------------------------"
        ;;
     k) sysinfo=$(snmpwalk -v1 -c $c $t 1.3.6.1.2.1.1.1 |cut -d ' ' -f4,5,6 |cut -d '"' -f2)
-           #This may need to be altered per Linux distro.
-           kern=$(echo "${sysinfo}" |cut -d ' ' -f3 |cut -d '.' -f1,2)
-           if [ -z "$kern" ]
-           then
-              printf ".\n.\nNo Kernel info found\n.\nTry a manual check\n.\n"
-           else
-                echo "--------------------------Kernel Research------------------------------"
-                echo "----------------------Searching: Kernel" "${kern}""---------------------------"
-                echo "-----------------------------------------------------------------------"
-                searchsploit -t kernel "${kern}" | grep -v 'windows/' | grep -v '/dos/' 
-           fi
+	   kern=$(echo "${sysinfo}" |cut -d ' ' -f3 |cut -d '.' -f1,2)
+	   if [ -z "$kern" ]
+	   then
+    	      printf ".\n.\nNo Kernel info found\n.\nTry a manual check\n.\n"
+	   else
+		echo "--------------------------Kernel Research------------------------------"
+		echo "----------------------Searching: Kernel" "${kern}""---------------------------"
+		echo "-----------------------------------------------------------------------"
+    	        searchsploit -t kernel "${kern}" | grep -v 'windows/' | grep -v '/dos/' 
+	   fi
 
        ;;
     o)  echo "Testing OS.."
-        os=$(snmpwalk -v1 -c $c $t 1.3.6.1.2.1.1.1 |cut -d ' ' -f4,5,6,7 |cut -d '"' -f2)
-        centos=$(cat snmp_tmp.txt |grep -i release |cut -c 16-18 |tr '-' '.')
-        ubuntu=$(cat snmp_tmp.txt |grep -i "ubuntu-docs" |cut -c 13-17)
-        debian=$(cat snmp_tmp.txt |grep -i "lsb-release" |cut -c 13-15)
-        #Added FreeBSD
-        freebsd=$(echo "$os" |grep -i release |cut -d ' ' -f4 |cut -d '-' -f1)
-        
-        #OS string counts
-        cntcen=$(cat snmp_tmp.txt |grep -c "centos")
-        cntubu=$(cat snmp_tmp.txt |grep -ic "ubuntu")
-        cntdeb=$(cat snmp_tmp.txt |grep -ic "debian")
-        #Added FreeBSD
-        cntbsd=$(cat snmp_tmp.txt |grep -ic "bsd")
-        
-        echo "-----------------------------------------------------------------------"
-        echo "Found ${cntcen} instances of CentOS strings in host resources"
-        echo "Found ${cntdeb} instances of Debian strings in host resources"
-        echo "Found ${cntubu} instances of Ubuntu strings in host resources"
-        echo "Found ${cntbsd} instances of BSD string in host resources"
-        echo "-----------------------------------------------------------------------"
-        
-        echo "System Description: ${os}"
-        sysname=$(snmpwalk -v1 -c $c $t 1.3.6.1.2.1.1.5 |cut -d ' ' -f3,4 |cut -d '"' -f2)
-        echo "System Name: ${sysname}"
-                case "$os" in
-                *ubuntu*)
-                        echo "Extremely likely target is Ubuntu ${ubuntu}"
-                        searchsploit -t ubuntu "${ubuntu}"
-                ;;
-                *debian*)
-                        echo "Extremely likely target is Debian ${debian}"
-                        searchsploit -t debian "${debian}"
-                ;;
-                *centos*)
-                        echo "Extremely likely target is CentOS ${centos}"
-                        searchsploit -t centos "${centos}" | grep -v '/windows/' | grep -v '/dos/'
-                ;;
-                *FreeBSD*)
-                        echo "Extremely likely target is FreeBSD ${freebsd}"
-                        searchsploit -t freebsd "${freebsd}" | grep -v '/windows/' | grep -v '/dos/'
-                ;;
-                *)
-                        echo "Guessing OS based off running procs..."
-                        if [ "${cntcen}" -gt "15" ]
-                        then
-                          echo "Highly likely CentOS ${centos}"
-                          searchsploit -t centos "${centos}" | grep -v '/windows/' | grep -v '/dos/'
-                        elif [ "${cntdeb}" -gt "15" ]
-                        then
-                          echo "Highly likely Debian ${debian}"
-                          searchsploit -t debian "${debian}"
-                        elif [ "${cntubu}" -gt "15" ]
-                        then
-                          echo "Highly likely Ubuntu ${ubuntu}"
-                          searchsploit -t ubuntu "${ubuntu}"
-                        elif [ "${cntbsd}" -gt "15" ]
-                        then
-                          echo "Highly likely FreeBSD ${freebsd}"
-                          searchsploit -t freebsd "${freebsd}"
-                        else
-                          echo "OS Unknown."
-                        fi
+	os=$(snmpwalk -v1 -c $c $t 1.3.6.1.2.1.1.1 |cut -d ' ' -f4,5,6,7 |cut -d '"' -f2)
+	centos=$(cat snmp_tmp.txt |grep -i release |cut -c 16-18 |tr '-' '.')
+	ubuntu=$(cat snmp_tmp.txt |grep -i "ubuntu-docs" |cut -c 13-17)
+	debian=$(cat snmp_tmp.txt |grep -i "lsb-release" |cut -c 13-15)
+	#OS string counts
+	cntcen=$(cat snmp_tmp.txt |grep -ic "centos")
+	cntubu=$(cat snmp_tmp.txt |grep -ic "ubuntu")
+	cntdeb=$(cat snmp_tmp.txt |grep -ic "debian")
+
+	cnt=("${cntdeb}" "${cntubu}" "${cntcen}")
+
+	echo "-----------------------------------------------------------------------"
+	echo "Found ${cntcen} instances of CentOS strings in host resources"
+	echo "Found ${cntdeb} instances of Debian strings in host resources"
+	echo "Found ${cntubu} instances of Ubuntu strings in host resources"
+	echo "-----------------------------------------------------------------------"
+	echo "System Description: ${os}"
+	sysname=$(snmpwalk -v1 -c $c $t 1.3.6.1.2.1.1.5 |cut -d ' ' -f3,4 |cut -d '"' -f2)
+	echo "System Name: ${sysname}"
+		case "$os" in
+		*ubuntu*)
+			echo "Extremely likely target is Ubuntu ${ubuntu}"
+			searchsploit -t ubuntu "${ubuntu}"
+		;;
+		*debian*)
+			echo "Extremely likely target is Debian ${debian}"
+			searchsploit -t debian "${debian}"
+		;;
+		*centos*)
+			echo "Extremely likely target is CentOS ${centos}"
+			searchsploit -t centos "${centos}" | grep -v '/windows/' | grep -v '/dos/'
+		;;
+		*)
+			echo "Guessing OS based off of running procs..."
+		        max=0
+		        for v in ${cnt[@]}; do
+          		if (( $v > $max )); then max=$v; fi;
+        		done
+
+		        if [ $max -eq "${cnt[0]}" ]; then
+				echo "Highly likely Debian ${debian}"
+				searchsploit -t debian "${debian}" |grep -v '/windows/' | grep -v '/dos/'
+        		elif [ $max -eq "${cnt[1]}" ]; then 
+				echo "Highly likely Ubuntu ${ubuntu}"
+				searchsploit -t ubuntu "${ubuntu}" |grep -v '/windows/' | grep -v '/dos/'
+	       		elif [ $max -eq "${cnt[2]}" ]; then
+				echo "Highly likely CentOS ${centos} "
+				searchsploit -t centos "${centos}" |grep -v '/windows/' | grep -v '/dos/'
+       			else
+               			echo "Uknown"
+       			fi
 
                 ;;
-                esac
-        echo "-----------------------------------------------------------------------"
-        ;;
-    n)  echo "----------Listing Aftermarket Processes (& some stock procs)----------" 
-        echo "-----------------------------------------------------------------------"
-        proc=$(snmpwalk -v1 -c $c $t .1.3.6.1.2.1.25.4.2.1.2 |cut -d ':' -f2 |cut  -d '"' -f2)
-        
-        #Add strings here to remove from -n switch
-        grep -r "${proc}" "snmp_tmp.txt" |sort -u |awk '!/lib/ &&! /gnome/ && !/colord/  && !/fonts/ && !/b$
-        && !/indicator/ && !/unity/ && !/update/ && !/whoopsie/ && !/python3/ && !/cups/ && !/compiz/ \
-        && !/nautilus/ && !/shotwell/ && !/bamf/ && !/lshw/ && !/session/ && !/dconf/ && !/toshset/ && !/fw$
-        && !/shared-mime/ && !/lightdm/ && !/anacron/ && !/wbritish/ && !/upstart/ && !/bash-completion/ &&$
-        && !/zeitgeist/ && !/xorg/ && !/pulseaudio-[a-z]/ && !/python-[a-z]/ && !/gdm-user/ && !/rpm-/ && !$
-        && !/mingetty/ && !/metacity/ && !/gnote/ && !/tracker-/ && !/gstreamer/ && !/multiarch/ && !/mime/$
-        echo "-----------------------------------------------------------------------"
+		esac
+	echo "-----------------------------------------------------------------------"
+	;;
+    n)  echo "----------Listing Aftermarket Processes (& some stock procs)-----------"
+	echo "-----------------------------------------------------------------------"
+	proc=$(snmpwalk -v1 -c $c $t .1.3.6.1.2.1.25.4.2.1.2 |cut -d ':' -f2 |cut  -d '"' -f2)
+	#Strings that eliminante some stock procs
+	grep -r "${proc}" "snmp_tmp.txt" |sort -u |awk '!/lib/ &&! /gnome/ && !/colord/  && !/fonts/ && !/bluetooth/ \
+	&& !/indicator/ && !/unity/ && !/update/ && !/whoopsie/ && !/python3/ && !/cups/ && !/compiz/ \
+	&& !/nautilus/ && !/shotwell/ && !/bamf/ && !/lshw/ && !/session/ && !/dconf/ && !/toshset/ && !/fwupd/ \
+	&& !/shared-mime/ && !/lightdm/ && !/anacron/ && !/wbritish/ && !/upstart/ && !/bash-completion/ && !/snapd/ \ 
+	&& !/zeitgeist/ && !/xorg/ && !/pulseaudio-[a-z]/ && !/python-[a-z]/ && !/gdm-user/ && !/rpm-/ && !/sysvinit/ \
+	&& !/mingetty/ && !/metacity/ && !/gnote/ && !/tracker-/ && !/gstreamer/ && !/multiarch/ && !/mime/ \
+	&& !/psutils/ && !/pm-utils/ && !/sudo-/ && !/dhclient/ && !/nss-sysin/ && !/wpa_suppli/ && !/initscripts/ \
+	&& !/dnsmasq/ && !/dbus-/ && !/systemd-/ && !/dash-/ && !/bash-/ && !/squashfs-/ && !/acpid-/  '
+	echo "-----------------------------------------------------------------------"
        ;;
     h)  echo "${usage}"
-        exit 1
+	exit 1
        ;;
     b)  b="${OPTARG}"
-        #brute force using nmap script. Update here if you'd like to change wordlist.
-        nmap --script snmp-brute.nse -sU $b -p161 --script-args snmpbrute.communitiesdb=/usr/share/seclists$
-        ;;
+	#brute force using nmap script
+	nmap --script snmp-brute.nse -sU $b -p161 --script-args snmpbrute.communitiesdb=/usr/share/seclists/Miscellaneous/snmp.txt 
+	;;
     :) echo "Argument needs to be added after switch" ;;
     \?) error "Unexpected option ${flag}" ;;
   esac
 done
 shift $((OPTIND - 1))
 
-
-# Cleaning up
+#Removing temp file  if it exits
 if [ -f snmp_tmp.txt ]; then
    rm snmp_tmp.txt
 fi
 
 echo "-------------------------It's Judgment Day-----------------------------"
 echo "-----------------------------------------------------------------------"
-echo "-------------------------snmprock complete----------------------------"
+echo "-------------------------snmprock complete-----------------------------"
 echo "-----------------------------------------------------------------------"
